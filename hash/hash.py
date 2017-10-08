8ce7ba5cd950b85be6200b4947328bd71af0e60f
David Han <dth2126@columbia.edu>
2017-10-08 18:45:49 -0400

    third

---
+++ hash/hash.py
@@ -0,0 +1,11 @@
+name = "david"
+list1 = []
+for c in name:
+    list1.append(ord(c) % 17)
+
+nameLen = len(name)
+for i in range(nameLen):
+    print ("key: %s | keyValue: %s | index: %s" % (name[i],ord(name[i]),list1[i]))
+
+
+
