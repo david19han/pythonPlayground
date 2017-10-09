4ef84a66c57e9cab06e2c82bf2ece2be00b3d177
David Han <davidhan@dyn-209-2-208-134.dyn.columbia.edu>
2017-10-08 21:28:12 -0400

    transfer

--- hash/hashCUDA.py
+++ hash/hashCUDA.py
@@ -15,7 +15,7 @@ import pycuda.autoinit
 kernel_code_template = """
 __global__ void findHash(char *a, int *c)
 {
-    int i = blockIdx.x * blockDim.x + threadidx.x;
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = (int) a[i];
     c[i] = j % 17;
 }
