d08ae4553764dd0c566a04b19cae74388945b3d6
David Han <dth2126@columbia.edu>
2017-11-10 22:15:31 -0500

    d

--- assignment3/2DConvCUDA.py
+++ assignment3/2DConvCUDA.py
@@ -15,9 +15,9 @@ kernel_code_template = """
 #include <stdio.h>
 __global__ void dilateConv(float *input,float *output,float *mask, int maskWidth, int width, int height)
 {
-    int tx = threadIdx.x
-    int ty = threadIdy.y
-    printf("tx is %d | ty is %d",tx,ty)
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    printf("tx is %d | ty is %d",tx,ty);
 }
 """
 
@@ -26,9 +26,9 @@ numRows = 7
 numCols = 7
 stride = 1
 
-input_cpu = np.random.rand(numRows,numCols)
-output_cpu = np.ones_like(input_cpu)
-mask_cpu = [[-1,-2,-1],[0,0,0],[1,2,1]]
+input_cpu = np.random.rand(numRows,numCols).astype(np.float32)
+output_cpu = np.ones_like(input_cpu).astype(np.float32)
+mask_cpu = np.array([[-1,-2,-1],[0,0,0],[1,2,1]]).astype(np.float32)
 
 input_gpu = gpuarray.to_gpu(input_cpu)
 output_gpu = gpuarray.to_gpu(output_cpu)
@@ -40,7 +40,7 @@ dilateConv = mod.get_function("dilateConv")
 
 block = (numRows,numCols,1)
 grid = (1,1)
-findhash(
+dilateConv(
     # inputs
     input_gpu,
     output_gpu,
@@ -48,5 +48,5 @@ findhash(
     maskWidth,
     numCols,
     numRows,
-    block=block,grid=grid
+    block = block,
 )
