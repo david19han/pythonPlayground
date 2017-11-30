b394131990e92ede21393f8e9833e823caa0a878
David Han <davidhan@Davids-MacBook-Pro.local>
2017-11-30 18:25:46 -0500

    transfer

--- assignment4/histCL.py
+++ assignment4/histCL.py
@@ -215,30 +215,7 @@ __kernel void func(__global int* data, __global int* histogram, int size) {
         localHisto[i] = 0;
     }
 
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    if(col<size && row<size){
-        int index = col + row * size;
-        int value = data[index];
-        int bIndex = value/10;
-        atomic_Add(&localHisto[bIndex],1);
-    }
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-   //index into right 18 bin set 
-
-    if(get_local_id(0) < 19 && get_local_id(1)==0){
-
-        int rowRegion = row/1024;
-        int colRegion = col/1024;
-
-        int numBox = size/1024;
-        int binRegion = colRegion + rowRegion * numBox;
-        int gIndex = get_local_id(0) + binRegion*18;
-
-        atomicAdd(&histogram[gIndex],localHisto[get_local_id(0)]);
-    }
+   
 }
 """
 
