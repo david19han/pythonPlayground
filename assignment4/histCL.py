4aba2534f8c9656bb4c67ed147955434669ba9c0
David Han <davidhan@Davids-MacBook-Pro.local>
2017-11-30 17:43:18 -0500

    transfer

--- assignment4/histCL.py
+++ assignment4/histCL.py
@@ -120,36 +120,35 @@ queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_EN
 
 # Define the OpenCL kernel you wish to run; most of the interesting stuff you
 # will be doing involves modifying or writing kernels:
-# naiveKernel = """
-# __kernel void func(__global int* data, __global int* histogram, int size) {
-#     int col = get_group_id(0) * get_local_size(0) + get_local_id(0);
-#     int row = get_group_id(1) * get_local_size(1) + get_local_id(1);
-    
-#     if(col<size && row < size){
-#         int index = col + row * size;
-#         int value = data[index];
-#         int bIndex = value/10;
-
-#         int rowRegion = row/1024;
-#         int colRegion = col/1024;
-
-#         int numBox = size/1024;
-
-#         int binRegion = colRegion + rowRegion * numBox;
-#         bIndex += binRegion*18;
-
-#         atomic_add(&histogram[bIndex],1);
-#     }
-# }
-# """
-
 naiveKernel = """
-__kernel void func(__global int* histogram) {
-    int j = get_group_id(0)*get_group_size(0) + get_local_id(0);
+__kernel void func(__global int* data, __global int* histogram, int size) {
+    int col = get_group_id(0) * get_local_size(0) + get_local_id(0);
+    int row = get_group_id(1) * get_local_size(1) + get_local_id(1);
+    
+    if(col<size && row < size){
+        int index = col + row * size;
+        int value = data[index];
+        int bIndex = value/10;
 
-    histogram[j] = j;
+        int rowRegion = row/1024;
+        int colRegion = col/1024;
+
+        int numBox = size/1024;
+
+        int binRegion = colRegion + rowRegion * numBox;
+        bIndex += binRegion*18;
+
+        atomic_add(&histogram[bIndex],1);
+    }
 }
 """
+
+# naiveKernel = """
+# __kernel void func(__global int* histogram) {
+#     int j = get_group_id(0)*get_group_size(0) + get_local_id(0);
+#     histogram[j] = j;
+# }
+# """
 
 
 print("Sequential 2^10x2^10")
@@ -185,12 +184,9 @@ output_gpu_small = cl.array.empty(queue, (18,), 'int32')
 
 prg = cl.Program(ctx, naiveKernel).build()
 
-# prg.func(queue,(smallMatrix,smallMatrix),(32,32),input_gpu_small.data,output_gpu_small.data,np.int32(smallMatrix))
-# print(np.array_equal(output_gpu_small.get(),hgram10.astype('int32')))
-# CustomPrintHistogram(output_gpu_small.get()[:18])
-
-prg.func(queue,(18,1),(18,1),output_gpu_small.data)
-print(output_gpu_small.get())
+prg.func(queue,(smallMatrix,smallMatrix),(32,32),input_gpu_small.data,output_gpu_small.data,np.int32(smallMatrix))
+print(np.array_equal(output_gpu_small.get(),hgram10.astype('int32')))
+CustomPrintHistogram(output_gpu_small.get()[:18])
 
 
 # print("Naive GPU for Medium Matrix:")
