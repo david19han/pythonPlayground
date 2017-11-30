c7f4661f59932898c803c294b592b6118a777889
David Han <davidhan@Davids-MacBook-Pro.local>
2017-11-30 18:22:58 -0500

    transfer

--- assignment4/histCL.py
+++ assignment4/histCL.py
@@ -204,41 +204,47 @@ largeMatrix = np.power(2,15)
 # print(np.array_equal(output_gpu_large.get(),hgram15.astype('int32')))
 
 
+# optKernel = """
+# __kernel void func(__global int* data, __global int* histogram, int size) {
+#     int col = get_group_id(0) * get_local_size(0) + get_local_id(0);
+#     int row = get_group_id(1) * get_local_size(1) + get_local_id(1);
+
+#     __local int localHisto[18];
+
+#     for(int i = 0;i<18;i++){
+#         localHisto[i] = 0;
+#     }
+
+#     barrier(CLK_LOCAL_MEM_FENCE);
+
+#     if(col<size && row<size){
+#         int index = col + row * size;
+#         int value = data[index];
+#         int bIndex = value/10;
+#         atomic_Add(&localHisto[bIndex],1);
+#     }
+
+#     barrier(CLK_LOCAL_MEM_FENCE);
+
+#    //index into right 18 bin set 
+
+#     if(get_local_id(0) < 19 && get_local_id(1)==0){
+
+#         int rowRegion = row/1024;
+#         int colRegion = col/1024;
+
+#         int numBox = size/1024;
+#         int binRegion = colRegion + rowRegion * numBox;
+#         int gIndex = get_local_id(0) + binRegion*18;
+
+#         atomicAdd(&histogram[gIndex],localHisto[get_local_id(0)]);
+#     }
+# }
+# """
+
 optKernel = """
 __kernel void func(__global int* data, __global int* histogram, int size) {
-    int col = get_group_id(0) * get_local_size(0) + get_local_id(0);
-    int row = get_group_id(1) * get_local_size(1) + get_local_id(1);
-
-    __local int localHisto[18];
-
-    for(int i = 0;i<18;i++){
-        localHisto[i] = 0;
-    }
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-    if(col<size && row<size){
-        int index = col + row * size;
-        int value = data[index];
-        int bIndex = value/10;
-        atomic_Add(&localHisto[bIndex],1);
-    }
-
-    barrier(CLK_LOCAL_MEM_FENCE);
-
-   //index into right 18 bin set 
-
-    if(get_local_id(0) < 19 && get_local_id(1)==0){
-
-        int rowRegion = row/1024;
-        int colRegion = col/1024;
-
-        int numBox = size/1024;
-        int binRegion = colRegion + rowRegion * numBox;
-        int gIndex = get_local_id(0) + binRegion*18;
-
-        atomicAdd(&histogram[gIndex],localHisto[get_local_id(0)]);
-    }
+ 
 }
 """
 
